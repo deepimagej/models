156e65765e2324da7ee6f93aa755cc0fe9ad02eb
esgomezm <43643518+esgomezm@users.noreply.github.com>
2021-07-23 17:24:00 +0200

    Update create_deepimagej_macro.py

--- src/create_deepimagej_macro.py
+++ src/create_deepimagej_macro.py
@@ -3,9 +3,34 @@ from ruamel.yaml import YAML
 import numpy as np
 import urllib.request
 
-yaml_url = "https://sandbox.zenodo.org/record/885236/files/model.yaml"
-urllib.request.urlretrieve(yaml_url,"model.yaml")
+# yaml_url = "https://sandbox.zenodo.org/record/885236/files/model.yaml"
+# Constantin's magic:
+def enforce_min_shape(min_shape, step, axes):
+    """Hack: pick a bigger shape than min shape
+    Some models come with super tiny minimal shapes, that make the processing
+    too slow. While dryrun is not implemented, we'll "guess" a sensible shape
+    and hope it will fit into memory.
+    """
+    MIN_SIZE_2D = 256
+    MIN_SIZE_3D = 64
+
+    assert len(min_shape) == len(step) == len(axes)
+
+    spacial_increments = sum(i != 0 for i, a in zip(step, axes) if a in "xyz")
+    if spacial_increments > 2:
+        target_size = MIN_SIZE_3D
+    else:
+        target_size = MIN_SIZE_2D
+
+    factors = [math.ceil((target_size - s) / i) for s, i, a in zip(min_shape, step, axes) if a in "xyz"]
+    if sum(f > 0 for f in factors) == 0:
+        return min_shape
+
+    m = max(factors)
+    return [s + i * m for s, i in zip(min_shape, step)]
+
 def create_dij_macro(url):
+    urllib.request.urlretrieve(yaml_url, "model.yaml")
     try:
         yaml = YAML()
     with open('model.yaml') as f:
@@ -32,33 +57,40 @@ def create_dij_macro(url):
     axes = ','.join(axes)
     input_shape = YAML_dict['inputs'][0]['shape']
 
-
-    test_image = np.load( YAML_dict['test_inputs'][0])
-    dims = [test_image.shape[d] for d in range(len(test_image.shape))]
-    # shape = min + (n)*step
     if 'min' in input_shape:
-        shape = [] # variable to store the shape of the tile
         m = input_shape['min']
         step = input_shape['step']
-        for i in range(len(d)):
-            if YAML_dict['inputs'][0]['axes'].__contains__('b'):
-                # avoid the batch dimension
-                j = i+1
-            else:
-                j = 1
-            if step[j] > 0:
-                #if the step is 0, then the shape is like min
-                n = np.floor((d[i]-m[j])/step[j]) # multiple for the shape calculator
-                shape.append(int(m[j] + n*step[j]))
-            else:
-                shape.append(int(m[j]))
+        shape = enforce_min_shape(m, step, axes) # ensure that the tile shape won't be too large
+        # shape = [] # variable to store the shape of the tile
+        # test_image = np.load( YAML_dict['test_inputs'][0])
+        # dims = [test_image.shape[d] for d in range(len(test_image.shape))]
+        # for i in range(len(dims)):
+            # if YAML_dict['inputs'][0]['axes'].__contains__('b'):
+            #     # avoid the batch dimension
+            #     j = i+1
+            # else:
+            #     j = 1
+            # if step[j] > 0:
+            #     #if the step is 0, then the shape is like min
+            #     n = np.floor((d[i]-m[j])/step[j]) # multiple for the shape calculator
+            #     shape.append(int(m[j] + n*step[j]))
+            # else:
+            #     shape.append(int(m[j]))
         shape = ','.join([str(i) for i in shape])
+    else:
+        if YAML_dict['inputs'][0]['axes'].__contains__('b'):
+            input_shape = input_shape[1:]
+        shape = ','.join([str(i) for i in input_shape])
+        
+    if 'tensorflow_saved_model_bundle' in YAML_dict['weights']:
+        format = "Tensorflow"
+    elif 'pytorch_script' in YAML_dict['weights']:
+        format = "Pytorch"
+    else:
+        print("This models does not have any deepImageJ compatible weight format.")
 
     ijmacro = f"""
     run("DeepImageJ Run",
-        "model=[{model_name}] format=Tensorflow preprocessing=[{preprocessing_txt}] postprocessing=[{postprocessing_txt}] axes={axes} tile={shape} logging=normal");
+        "model=[{model_name}] format={format} preprocessing=[{preprocessing_txt}] postprocessing=[{postprocessing_txt}] axes={axes} tile={shape} logging=normal");
     """
     return ijmacro
-
-
-
