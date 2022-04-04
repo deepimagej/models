b71aff08b806433252ce568d814e9c10b2433f05
carlosuc3m <49989524+carlosuc3m@users.noreply.github.com>
2022-04-04 21:58:30 +0200

    add test macro

--- scripts/run_deepimagej_in_python.py
+++ scripts/run_deepimagej_in_python.py
@@ -72,11 +72,14 @@ def run_model_with_deepimagej(fiji_dir: Path,
     ij = imagej.init(os.path.expanduser(fiji_dir) + '/Fiji.app',headless=True)
     ij.getVersion()
 
+    # Check that the example tif exists
 
 
     macrotest = """
-        run ("Blobs");
-        run ("Gaussian Blur...", "sigma=2");
+        open("/home/runner/Fiji.app/models/DEFCoN.bioimage.io.model/exampleImage.tif");
+        selectWindow("exampleImage.tif");
+        run("DeepImageJ Run", "model=[SMLM Density Map Estimation (DEFCoN)] format=Tensorflow preprocessing=[no preprocessing] postprocessing=[no postprocessing] axes=Y,X,C tile=84,84,1 logging=normal models_dir=/home/runner/Fiji.app/models");
+        saveAs("PNG", "/home/runner/Fiji.app/models/DEFCoN.bioimage.io.model/test_output.png");
     """
 
     IJ.runMacro (macrotest)
