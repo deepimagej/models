1877d88938b7bc420a5dfc47e70a5f3c4eaa90b9
carlosuc3m <49989524+carlosuc3m@users.noreply.github.com>
2022-04-04 23:57:54 +0200

    add methods to download models into FIJI

--- scripts/run_deepimagej_in_python.py
+++ scripts/run_deepimagej_in_python.py
@@ -3,6 +3,8 @@ import os
 import traceback
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
+import os
+import requests
 
 import numpy
 import numpy as np
@@ -24,6 +26,39 @@ from bioimageio.spec.model.raw_nodes import WeightsFormat
 from bioimageio.spec.shared.raw_nodes import ResourceDescription as RawResourceDescription
 
 from create_dij_macro immport create_dij_macro
+
+
+def download_deepimagej_model(fiji_dir: Path, rdf: YAML_dict):
+    # Create the model folder
+    os.makedirs(fiji_dir + "//models")
+    model_name = rdf.get("name")
+    model_dir = fiji_dir + "//models//" + model_name
+    os.mkdir(model_dir)
+    # Download the files
+    download(rdf.get(""), model_dir)
+
+def download(url: str, dest_folder: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    filename = url.split('/')[-1].replace(" ", "_") 
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+        return None
+    else:  # HTTP status code 4XX/5XX
+        error = "Download failed: status code {}\n{}".format(r.status_code, r.text)
+        print(error)
+        return error
+
 
 
 def run_model_with_deepimagej(fiji_dir: Path, 
