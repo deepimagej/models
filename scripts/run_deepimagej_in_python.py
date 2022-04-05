b63f2f3964017d56e00cac7be311fa20a5f9c8aa
carlosuc3m <49989524+carlosuc3m@users.noreply.github.com>
2022-04-05 13:19:42 +0200

    finish pethod to download models

--- scripts/run_deepimagej_in_python.py
+++ scripts/run_deepimagej_in_python.py
@@ -39,18 +39,69 @@ def download_deepimagej_model(fiji_dir: Path, rdf: YAML_dict):
     if (model_download not None):
         result = download(model_download, model_dir)
         if (result == None):
-            return
+            return None
     # If the model was not downloaded with a direct link, download all the needed files
-    attachments = rdf.get("attachments")
-    if (attachments not )
-    attachments = rdf.get("attachments").get("files")
-    for attach in attachments:
-        download_result = download(attach, model_dir)
-        if (download_result not None):
-            error = (error or "") + "Error downloading attachment " + str(attach) + ".\n"
-            + download_result + "\n"
+    
+    return result + "\n" + download_deepimagej_file_by_file(fiji_dir, rdf)
+
+
+
+
+
+
+
+def download(url: str, dest_folder: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    filename = url.split('/')[-1].replace(" ", "_") 
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+        return None
+    else:  # HTTP status code 4XX/5XX
+        error = "Download failed: status code {}\n{}".format(r.status_code, r.text)
+        print(error)
+        return error
+
+
+
+def run_model_with_deepimagej(fiji_dir: Path, 
+    model_rdf: Union[URI, Path, str],
+    weight_format: Optional[WeightsFormat] = None,
+    devices: Optional[List[str]] = None,
+    decimal: int = 4,
+) -> dict:
+
+    print(fiji_dir)
+    ij = imagej.init(os.path.expanduser(fiji_dir) + '/Fiji.app',headless=True)
+    ij.getVersion()
+
+    # Check that the example tif exists
+
+
+    macrotest = """
+        open("/home/runner/Fiji.app/models/DEFCoN.bioimage.io.model/exampleImage.tif");
+        selectWindow("exampleImage.tif");
+        run("DeepImageJ Run", "model=[SMLM Density Map Estimation (DEFCoN)] format=Tensorflow preprocessing=[no preprocessing] postprocessing=[no postprocessing] axes=Y,X,C tile=84,84,1 logging=normal models_dir=/home/runner/Fiji.app/models");
+        saveAs("PNG", "/home/runner/Fiji.app/models/DEFCoN.bioimage.io.model/test_output.png");
+    """
+
+    IJ.runMacro (macrotest)
+
+
+
 
 def download_deepimagej_file_by_file(fiji_dir: Path, rdf: YAML_dict):
+    error = None
     # Create the model folder
     os.makedirs(fiji_dir + "//models")
     model_name = rdf.get("name")
@@ -119,54 +170,17 @@ def download_deepimagej_file_by_file(fiji_dir: Path, rdf: YAML_dict):
                 + download_result + "\n"
     else:
         error = (error or "") + "rdf.yaml missing 'test_outputs' field.\n"
-
-
-
-
-
-def download(url: str, dest_folder: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)  # create folder if it does not exist
-
-    filename = url.split('/')[-1].replace(" ", "_") 
-    file_path = os.path.join(dest_folder, filename)
-
-    r = requests.get(url, stream=True)
-    if r.ok:
-        print("saving to", os.path.abspath(file_path))
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
-        return None
-    else:  # HTTP status code 4XX/5XX
-        error = "Download failed: status code {}\n{}".format(r.status_code, r.text)
-        print(error)
-        return error
-
-
-
-def run_model_with_deepimagej(fiji_dir: Path, 
-    model_rdf: Union[URI, Path, str],
-    weight_format: Optional[WeightsFormat] = None,
-    devices: Optional[List[str]] = None,
-    decimal: int = 4,
-) -> dict:
-
-    print(fiji_dir)
-    ij = imagej.init(os.path.expanduser(fiji_dir) + '/Fiji.app',headless=True)
-    ij.getVersion()
-
-    # Check that the example tif exists
-
-
-    macrotest = """
-        open("/home/runner/Fiji.app/models/DEFCoN.bioimage.io.model/exampleImage.tif");
-        selectWindow("exampleImage.tif");
-        run("DeepImageJ Run", "model=[SMLM Density Map Estimation (DEFCoN)] format=Tensorflow preprocessing=[no preprocessing] postprocessing=[no postprocessing] axes=Y,X,C tile=84,84,1 logging=normal models_dir=/home/runner/Fiji.app/models");
-        saveAs("PNG", "/home/runner/Fiji.app/models/DEFCoN.bioimage.io.model/test_output.png");
-    """
-
-    IJ.runMacro (macrotest)
+    # Download the weights
+    weights = rdf.get("weights")
+    if (weights not None and type(weights) == dict):
+        for kk, vv in weights.items():
+            if (type(vv) == dict and vv.get("source") not None):
+                download_result = download(vv.get("source"), model_dir)
+                if (download_result not None):
+                    error = (error or "") + "Error downloading weights " + str(vv.get("source")) + ".\n"
+                    + download_result + "\n"
+            else:
+                error = (error or "") + "error downloading weights.\n"
+    else:
+        error = (error or "") + "rdf.yaml missing 'weights' field.\n"
+    return error
