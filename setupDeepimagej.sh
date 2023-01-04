6259f76df55ae002d50cd19d1bd23333a2230aed
Lucía Moya-Sans <44240573+lmoyasans@users.noreply.github.com>
2023-01-04 13:23:39 +0100

    Add handle for rm command error and fix mv versions

--- setupDeepimagej.sh
+++ setupDeepimagej.sh
@@ -1,18 +1,21 @@
-wget https://downloads.imagej.net/fiji/archive/20222101-1017/fiji-linux64.zip
+#Remove previous installations of Fiji if they exist
+rm -rf $HOME/Fiji.app/
+wget https://downloads.imagej.net/fiji/archive/20221201-1017/fiji-linux64.zip
 unzip fiji-linux64.zip -d $HOME
-# fix FilamentDetector issue
-mv $HOME/Fiji.app/jars/FilamentDetector-1.0.0.jar $HOME/Fiji.app/jars/FilamentDetector-1.0.0.jar.disabled
+# fix FilamentDetector issue (not necessary anymore, non existent file in the last version)
+#mv $HOME/Fiji.app/jars/FilamentDetector-1.0.0.jar $HOME/Fiji.app/jars/FilamentDetector-1.0.0.jar.disabled
 ##$HOME/Fiji.app/ImageJ-linux64 --update add-update-site DeepImageJ https://sites.imagej.net/DeepImageJ/
 ##$HOME/Fiji.app/ImageJ-linux64 --update update
+#rm $HOME/Fiji.app/jars/jna-4*.jar
 
-rm $HOME/Fiji.app/jars/jna-4*.jar
 wget https://github.com/deepimagej/deepimagej-plugin/releases/download/2.1.15/dependencies_2115.zip
-unzip dependencies_219.zip
-mv dependencies_219/dependencies_2.1.9/* $HOME/Fiji.app/jars/
+unzip dependencies_2115.zip
+mv dependencies_2115/dependencies_2.1.15/* $HOME/Fiji.app/jars/
 
 wget https://github.com/deepimagej/deepimagej-plugin/releases/download/2.1.15/DeepImageJ_-2.1.15.jar
-rm $HOME/Fiji.app/plugins/DeepImageJ*.jar
-mv DeepImageJ_-2.1.10.jar $HOME/Fiji.app/plugins/DeepImageJ_-2.1.15.jar
+#Add the -f mode to deal with the case of non existent diji instalations
+rm  -f $HOME/Fiji.app/plugins/DeepImageJ*.jar
+mv DeepImageJ_-2.1.15.jar $HOME/Fiji.app/plugins/DeepImageJ_-2.1.15.jar
 #python3 -c "import imagej;ij = imagej.init('$HOME/Fiji.app');print('pyimagej initialized.')"
 #export DISPLAY=:1
 #Xvfb $DISPLAY -screen 0 1024x768x16 &
